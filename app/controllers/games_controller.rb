ea5da2e187223732a0eadffe8c574d3b760a96db
Solene Baudic <solene.baudic@gmail.com>
2024-07-02 09:24:33 +0200

    EN COURS

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,31 +7,21 @@ class GamesController < ApplicationController
   end
 
   def score
-    # TODO: runs the game and return detailed hash of result (with `:score`, `:message` and `:time` keys)
-    @word = params[:word]
-    @grid = params[:grid].chars
-
-    if  valid_word_in_grid?(@word, @grid)
-      if english_word?(@word)
-        @message = "Congratulations! #{@word.upcase} is a valid English word!"
-      else
-        @message = "Sorry but #{@word.upcase} does not seem to be a valid English word..."
-      end
-    else
-      @message = "Sorry but #{@word.upcase} can't be build out of #{@grid.join(', ')}..."
-    end
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word
   end
 
   private
 
-  def valid_word_in_grid?(word, grid)
-    word.chars.all? { |letter| word.count(letter) <= grid.count(letter) }
+  def valid_word_in_grid?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
   end
 
   def english_word?(word)
-    url = "https://wagon-dictionary.herokuapp.com/#{word}"
-    response = URI.open(url).read
-    json = JSON.parse(response)
+    response = URI.open("https://dictionary.lewagon.com/#{word}")
+    json = JSON.parse(response.read)
     json['found']
   end
 
